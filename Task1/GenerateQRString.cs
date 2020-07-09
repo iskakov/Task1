4d30674f820a54fd51ff9a41debea595b3520252
ma3ca3@gmail.com <2121123Qq>
2020-07-09 10:47:43 +0500

    Добавил тестов исправил логику

--- Task1/GenerateQRString.cs
+++ Task1/GenerateQRString.cs
@@ -14,35 +14,54 @@ namespace Task1
         public static string FirstGenerate(string name, DateTime dateRegistration, string personalAccount)
         {
             string generateSt = "";
+            if (String.IsNullOrEmpty(name))
+            {
+                return "";
+            }
+            if (String.IsNullOrEmpty(personalAccount))
+            {
+                return "";
+            }
             string part1 = name.Length>=6 ?  name.Substring(name.Length/2-2,4) : name;
             string part2 = dateRegistration.ToShortDateString();
             generateSt += part1 + ";" + part2 + ";";
-            string part3 = generateSt.Length < 18 ? personalAccount.Substring(personalAccount.Length / 2, 17 - generateSt.Length) : "";
+            string part3 = personalAccount.Substring(personalAccount.Length / 2, 18 - generateSt.Length);
             generateSt += part3;
             return generateSt;
         }
 
         public static void FirstGetObject(string getnerateSt, out string name, out DateTime dateRegistration, out string personalAccount)
         {
-            var mass = getnerateSt.Split(';');
-            if(mass.Length == 1)
+            if (!String.IsNullOrEmpty(getnerateSt))
+            {
+                var mass = getnerateSt.Split(';');
+                if (mass.Length == 1)
+                {
+                    name = "";
+                    dateRegistration = new DateTime();
+                    personalAccount = "";
+                }
+                else
+                {
+                    name = mass[0];
+
+                    var prov = DateTime.TryParse(mass[1], out dateRegistration);
+
+                    if (mass.Length == 3)
+                        personalAccount = mass[2];
+                    else
+                    {
+                        personalAccount = "";
+                    }
+                }
+
+            }
+            else
             {
                 name = "";
                 dateRegistration = new DateTime();
                 personalAccount = "";
             }
-            else
-            {
-                name = mass[0];
-                dateRegistration = DateTime.Parse(mass[1]);
-                if (mass.Length == 3)
-                    personalAccount = mass[2];
-                else
-                {
-                    personalAccount = "";
-                }
-            }
-            
         }
 
         /**
@@ -50,6 +69,14 @@ namespace Task1
          */
         public static string SecondGenerate(string name, DateTime dateRegistration, string personalAccount)
         {
+            if (String.IsNullOrEmpty(name))
+            {
+                return "";
+            }
+            if (String.IsNullOrEmpty(personalAccount))
+            {
+                return "";
+            }
             string generateSt = "";
             string part1 = name.Length > 8 ? name.Substring(0, 8) : name;
             string part2 = personalAccount.Length > 8 ? personalAccount.Substring(0, 8) : personalAccount;
@@ -59,16 +86,23 @@ namespace Task1
 
         public static void SecondGetObject(string getnerateSt, out string name, out string personalAccount)
         {
-            var mass = getnerateSt.Split('-') ;
-            if (mass.Length == 1)
-            {
-                name = "";
-                personalAccount ="";
+            if (!String.IsNullOrEmpty(getnerateSt)) {
+                var mass = getnerateSt.Split('-');
+                if (mass.Length == 1)
+                {
+                    name = "";
+                    personalAccount = "";
+                }
+                else
+                {
+                    name = mass[0];
+                    personalAccount = mass[1];
+                }
             }
             else
             {
-                name = mass[0];
-                personalAccount = mass[1];
+                name = "";
+                personalAccount = "";
             }
         }
     }
---
+++ UnitTestForTask1/GenerateQRStringTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForTask1
+{
+    [TestClass]
+    public class GenerateQRStringTest
+    {
+        [TestMethod]
+        public void Generate_new_St_First()
+        {
+            string actual = "", expected = "ИЭТХ;24.04.2020;77";
+
+            actual = Task1.GenerateQRString.FirstGenerate("ОИЭТХФ", new DateTime(2020,4,24), "42867734");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Generate_new_St_First_With_error()
+        {
+            string actual = "", expected = "";
+
+            actual = Task1.GenerateQRString.FirstGenerate("", new DateTime(2020, 4, 24), "42867734");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Generate_new_St_First_With_error2()
+        {
+            string actual = "", expected = "";
+
+            actual = Task1.GenerateQRString.FirstGenerate("dfd", new DateTime(2020, 4, 24), "");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Generate_new_St_Second()
+        {
+            string actual = "", expected = "ОИЭТХФ-42867734";
+
+            actual = Task1.GenerateQRString.SecondGenerate("ОИЭТХФ", new DateTime(2020, 4, 24), "42867734");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Generate_new_St_Second_With_error()
+        {
+            string actual = "", expected = "";
+
+            actual = Task1.GenerateQRString.SecondGenerate("", new DateTime(2020, 4, 24), "42867734");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Generate_new_St_Second_With_error2()
+        {
+            string actual = "", expected = "";
+
+            actual = Task1.GenerateQRString.SecondGenerate("dfd", new DateTime(2020, 4, 24), "");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
