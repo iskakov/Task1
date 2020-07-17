e7348c4fa86a863508fe4e9baf1cb5b6f71f7a57
ma3ca3@gmail.com <2121123Qq>
2020-07-17 17:12:10 +0500

    Добавил описание к методам генерации + тесты на поиск объетка

--- UnitTestForTask1/GenerateQRStringTest.cs
+++ UnitTestForTask1/GenerateQRStringTest.cs
@@ -38,6 +38,54 @@ namespace UnitTestForTask1
 
 
         [TestMethod]
+        public void Get_Object_From_St_First()
+        {
+            string actualName = "", expectedName = "ИЭТХ";
+            string actualPerAcc = "", expectedPerAcc = "77";
+            string actualDate = "", expectDate= "24.04.2020";
+
+            DateTime dateTime;
+            Task1.GenerateQRString.FirstGetObject("ИЭТХ;24.04.2020;77", out actualName, out dateTime, out actualPerAcc);
+            actualDate = dateTime.ToShortDateString();
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expectedPerAcc, actualPerAcc);
+            Assert.AreEqual(expectDate, actualDate);
+
+        }
+        [TestMethod]
+        public void Get_Object_From_St_First_Error1()
+        {
+            string actualName = "", expectedName = "";
+            string actualPerAcc = "", expectedPerAcc = "";
+            string actualDate = "", expectDate = "";
+
+            DateTime dateTime;
+            Task1.GenerateQRString.FirstGetObject("", out actualName, out dateTime, out actualPerAcc);
+            if(!dateTime.Equals(new DateTime()))
+                actualDate = dateTime.ToShortDateString();
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expectedPerAcc, actualPerAcc);
+            Assert.AreEqual(expectDate, actualDate);
+        }
+      
+        [TestMethod]
+        public void Get_Object_From_St_First_Error2()
+        {
+
+            string actualName = "", expectedName = "";
+            string actualPerAcc = "", expectedPerAcc = "";
+            string actualDate = "", expectDate = "";
+            DateTime dateTime;
+            Task1.GenerateQRString.FirstGetObject("ИЭТХ-24.04.2020-77", out actualName, out dateTime, out actualPerAcc);
+            if (!dateTime.Equals(new DateTime()))
+                actualDate = dateTime.ToShortDateString();
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expectedPerAcc, actualPerAcc);
+            Assert.AreEqual(expectDate, actualDate);
+        }
+
+
+        [TestMethod]
         public void Generate_new_St_Second()
         {
             string actual = "", expected = "ОИЭТХФ-42867734";
@@ -66,5 +114,36 @@ namespace UnitTestForTask1
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Get_Object_From_St_Second()
+        {
+            string actualName = "", expectedName = "ОИЭТХФ";
+            string actualPerAcc = "", expectedPerAcc = "42867734";
+            Task1.GenerateQRString.SecondGetObject("ОИЭТХФ-42867734", out actualName, out actualPerAcc);
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expectedPerAcc, actualPerAcc);
+        }
+        [TestMethod]
+        public void Get_Object_From_St_Second_Error1()
+        {
+            string actualName = "", expectedName = "";
+            string actualPerAcc = "", expectedPerAcc = "";
+            Task1.GenerateQRString.SecondGetObject("", out actualName, out actualPerAcc);
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expectedPerAcc, actualPerAcc);
+        }
+
+        [TestMethod]
+        public void Get_Object_From_St_Second_Error2()
+        {
+            string actualName = "", expectedName = "";
+            string actualPerAcc = "", expectedPerAcc = "";
+            Task1.GenerateQRString.SecondGetObject("ОИЭТХФ:42867734", out actualName, out actualPerAcc);
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expectedPerAcc, actualPerAcc);
+        }
     }
+
+   
 }
